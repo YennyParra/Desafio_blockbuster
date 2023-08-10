fc3fa806f3f7140456409c8961f1b71e1a662aca
yennyparra <yennyparra79@gmail.com>
2023-08-10 19:18:17 -0400

    tabla movie

--- db/migrate/20230706004454_create_movies.rb
+++ db/migrate/20230706004454_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :name
-      t.references :client, null: true, foreign_key: true
+      t.references :client, foreign_key: true
 
       t.timestamps
     end
